4f3ca79689f2d01a4653dfbe5714701d724270ca
Cédric RENAIS <cedric.renais@gmail.com>
2024-07-30 20:15:41 +0200

    🚧 Nav en cours (JSX & CSS) 📱 Mobile version de la nav

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ const Home = () => {
       <h1 className="home__title u-head-text">
         Découvre & Partage
         <br className="home__title--break" />
-        <span className="home__title--highlight"> Les meilleurs Prompts</span>
+        <span className="home__title--highlight"> tes meilleurs Prompts</span>
       </h1>
       <p className="home__description u-desc-text">
         PromptPulse est une plateforme où tu peux découvrir, créer et partager
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,6 +13,9 @@ const Nav = () => {
   // Liste des fournisseurs d'authentification
   const [providers, setProviders] = useState(null);
 
+  // Toggle le dropdown du menu mobile
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
   // Récupère la liste des fournisseurs d'authentification
   useEffect(() => {
     const fetchProviders = async () => {
@@ -58,6 +61,64 @@ const Nav = () => {
                 height={35}
               />
             </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  className="nav__sign-in"
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                >
+                  Connecte-toi avec Google
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Nav - Mobile */}
+      <div className="nav__mobile">
+        {isUserLoggedIn ? (
+          <div className="nav__profile-mobile" tabIndex="0">
+            <Image
+              className="nav__profile-image"
+              src="/assets/images/user.svg"
+              alt="Image de profil"
+              width={35}
+              height={35}
+              onClick={() => setToggleDropdown((prevState) => !prevState)}
+            />
+            {toggleDropdown && (
+              <div className="nav__dropdown">
+                <Link
+                  className="nav__dropdown-link"
+                  href="/profile"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Ton profil
+                </Link>
+                <Link
+                  className="nav__dropdown-link"
+                  href="/create-post"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Pulse un prompt
+                </Link>
+                <button
+                  className="nav__dropdown-sign-out"
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                >
+                  Déconnecte-toi
+                </button>
+              </div>
+            )}
           </div>
         ) : (
           <>
